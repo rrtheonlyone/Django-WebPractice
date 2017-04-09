c47c231931219bc80ccce5d8ec9287b7d72bd477
rrtheonlyone <rahul.rajesh@hotmail.com>
2017-04-09 23:25:57 +0800

    Made progress on tutorial webpage with Django

--- website/music/models.py
+++ website/music/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Album(models.Model):
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
-    album_logo = models.CharField(max_length=100)
+    album_logo = models.FileField()
     
+    def get_absolute_url(self):
+        return reverse('music:detail', kwargs={'pk': self.pk})
+
     def __str__(self):
         return self.album_title + ' - ' + self.artist
 
@@ -13,3 +17,7 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title =models.CharField(max_length=250)
+    is_favourite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
--- website/music/views.py
+++ website/music/views.py
@@ -1,15 +1,27 @@
-from django.http import HttpResponse
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Album
 
-def index(request):
-    all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href = "' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+class IndexView(generic.ListView):
+	template_name = 'music/index.html'
+	context_object_name = 'albums'
 
-def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
-    
-    
+	def get_queryset(self):
+		return Album.objects.all()
+
+class DetailView(generic.DetailView):
+	model = Album
+	template_name = 'music/detail.html'
+
+class AlbumCreate(CreateView):
+	model = Album
+	fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumUpdate(UpdateView):
+	model = Album
+	fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumDelete(DeleteView):
+	model = Album
+	success_url = reverse_lazy('music:index')
